05e58f0bbda24ac51e6bee31616e977a1dde952e
Anthony <anthonyleonramirez@hotmail.com>
2020-03-09 11:31:36 -0500

    backup tetst

---
+++ docker/postgres_db/initdb/1-schema.sql
@@ -0,0 +1,413 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.11 (Debian 10.11-1.pgdg90+1)
+-- Dumped by pg_dump version 10.11 (Debian 10.11-1.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: auth; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA auth;
+
+
+ALTER SCHEMA auth OWNER TO postgres;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: auditing; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth.auditing (
+    id bigint NOT NULL,
+    data text,
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone,
+    input text,
+    id_user smallint NOT NULL,
+    id_auditing_type smallint NOT NULL
+);
+
+
+ALTER TABLE auth.auditing OWNER TO postgres;
+
+--
+-- Name: auditing_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth.auditing ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.auditing_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: auditing_type; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth.auditing_type (
+    id smallint NOT NULL,
+    name character varying(50),
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone
+);
+
+
+ALTER TABLE auth.auditing_type OWNER TO postgres;
+
+--
+-- Name: auditing_type_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth.auditing_type ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.auditing_type_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: object; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth.object (
+    id smallint NOT NULL,
+    name text,
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone,
+    id_object_type smallint NOT NULL
+);
+
+
+ALTER TABLE auth.object OWNER TO postgres;
+
+--
+-- Name: object_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth.object ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.object_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: object_type; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth.object_type (
+    id smallint NOT NULL,
+    name character varying(50),
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone
+);
+
+
+ALTER TABLE auth.object_type OWNER TO postgres;
+
+--
+-- Name: object_type_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth.object_type ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.object_type_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: permission; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth.permission (
+    id smallint NOT NULL,
+    name character varying(50),
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone,
+    id_permission_type smallint NOT NULL,
+    id_object smallint NOT NULL
+);
+
+
+ALTER TABLE auth.permission OWNER TO postgres;
+
+--
+-- Name: permission_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth.permission ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.permission_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: permission_type; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth.permission_type (
+    id smallint NOT NULL,
+    name character varying(50),
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone
+);
+
+
+ALTER TABLE auth.permission_type OWNER TO postgres;
+
+--
+-- Name: permission_type_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth.permission_type ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.permission_type_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: rol; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth.rol (
+    id smallint NOT NULL,
+    name text,
+    display_name character varying(50),
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone,
+    id_permission smallint NOT NULL
+);
+
+
+ALTER TABLE auth.rol OWNER TO postgres;
+
+--
+-- Name: rol_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth.rol ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.rol_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: user; Type: TABLE; Schema: auth; Owner: postgres
+--
+
+CREATE TABLE auth."user" (
+    id smallint NOT NULL,
+    username text,
+    password text,
+    names text,
+    surnames text,
+    created_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    modificated_date timestamp with time zone,
+    id_rol smallint NOT NULL
+);
+
+
+ALTER TABLE auth."user" OWNER TO postgres;
+
+--
+-- Name: user_id_seq; Type: SEQUENCE; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE auth."user" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME auth.user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: user User_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth."user"
+    ADD CONSTRAINT "User_pk" PRIMARY KEY (id);
+
+
+--
+-- Name: auditing auditing_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.auditing
+    ADD CONSTRAINT auditing_pk PRIMARY KEY (id);
+
+
+--
+-- Name: auditing_type auditing_types_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.auditing_type
+    ADD CONSTRAINT auditing_types_pk PRIMARY KEY (id);
+
+
+--
+-- Name: object_type object_type_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.object_type
+    ADD CONSTRAINT object_type_pk PRIMARY KEY (id);
+
+
+--
+-- Name: object objects_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.object
+    ADD CONSTRAINT objects_pk PRIMARY KEY (id);
+
+
+--
+-- Name: permission permissions_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.permission
+    ADD CONSTRAINT permissions_pk PRIMARY KEY (id);
+
+
+--
+-- Name: permission_type permissions_types_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.permission_type
+    ADD CONSTRAINT permissions_types_pk PRIMARY KEY (id);
+
+
+--
+-- Name: rol rol_pk; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.rol
+    ADD CONSTRAINT rol_pk PRIMARY KEY (id);
+
+
+--
+-- Name: user user_uq; Type: CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth."user"
+    ADD CONSTRAINT user_uq UNIQUE (id_rol);
+
+
+--
+-- Name: auditing auditing_type_fk; Type: FK CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.auditing
+    ADD CONSTRAINT auditing_type_fk FOREIGN KEY (id_auditing_type) REFERENCES auth.auditing_type(id) MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: permission object_fk; Type: FK CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.permission
+    ADD CONSTRAINT object_fk FOREIGN KEY (id_object) REFERENCES auth.object(id) MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: object object_type_fk; Type: FK CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.object
+    ADD CONSTRAINT object_type_fk FOREIGN KEY (id_object_type) REFERENCES auth.object_type(id) MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: rol permission_fk; Type: FK CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.rol
+    ADD CONSTRAINT permission_fk FOREIGN KEY (id_permission) REFERENCES auth.permission(id) MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: permission permission_type_fk; Type: FK CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.permission
+    ADD CONSTRAINT permission_type_fk FOREIGN KEY (id_permission_type) REFERENCES auth.permission_type(id) MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: user rol_fk; Type: FK CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth."user"
+    ADD CONSTRAINT rol_fk FOREIGN KEY (id_rol) REFERENCES auth.rol(id) MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: auditing user_fk; Type: FK CONSTRAINT; Schema: auth; Owner: postgres
+--
+
+ALTER TABLE ONLY auth.auditing
+    ADD CONSTRAINT user_fk FOREIGN KEY (id_user) REFERENCES auth."user"(id) MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ docker/postgres_db/initdb/2-data.sql
@@ -0,0 +1,150 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.11 (Debian 10.11-1.pgdg90+1)
+-- Dumped by pg_dump version 10.11 (Debian 10.11-1.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: auditing_type; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth.auditing_type (id, name, created_date, modificated_date) FROM stdin;
+1	AUDITING_TYPE TEST 1	2020-03-09 16:13:33.063784+00	\N
+\.
+
+
+--
+-- Data for Name: object_type; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth.object_type (id, name, created_date, modificated_date) FROM stdin;
+1	OBJECT_TYPE TEST 1	2020-03-09 16:09:34.867461+00	\N
+\.
+
+
+--
+-- Data for Name: object; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth.object (id, name, created_date, modificated_date, id_object_type) FROM stdin;
+2	OBJECT TEST 1	2020-03-09 16:10:11.043754+00	\N	1
+\.
+
+
+--
+-- Data for Name: permission_type; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth.permission_type (id, name, created_date, modificated_date) FROM stdin;
+1	PERMISSION_TYPE TEST 1	2020-03-09 16:10:51.525856+00	\N
+\.
+
+
+--
+-- Data for Name: permission; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth.permission (id, name, created_date, modificated_date, id_permission_type, id_object) FROM stdin;
+2	PERMISSION TEST 1	2020-03-09 16:12:03.931409+00	\N	1	2
+\.
+
+
+--
+-- Data for Name: rol; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth.rol (id, name, display_name, created_date, modificated_date, id_permission) FROM stdin;
+1	ROL TEST 1	TEST	2020-03-09 16:12:55.648846+00	\N	2
+\.
+
+
+--
+-- Data for Name: user; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth."user" (id, username, password, names, surnames, created_date, modificated_date, id_rol) FROM stdin;
+2	USER TEST 1	TEST	TEST	1	2020-03-09 16:14:59.665753+00	\N	1
+\.
+
+
+--
+-- Data for Name: auditing; Type: TABLE DATA; Schema: auth; Owner: postgres
+--
+
+COPY auth.auditing (id, data, created_date, modificated_date, input, id_user, id_auditing_type) FROM stdin;
+1	DATA TEST 1	2020-03-09 16:15:35.47442+00	\N	INPUT TEST 1	2	1
+\.
+
+
+--
+-- Name: auditing_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.auditing_id_seq', 1, true);
+
+
+--
+-- Name: auditing_type_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.auditing_type_id_seq', 1, true);
+
+
+--
+-- Name: object_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.object_id_seq', 2, true);
+
+
+--
+-- Name: object_type_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.object_type_id_seq', 1, true);
+
+
+--
+-- Name: permission_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.permission_id_seq', 2, true);
+
+
+--
+-- Name: permission_type_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.permission_type_id_seq', 1, true);
+
+
+--
+-- Name: rol_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.rol_id_seq', 1, true);
+
+
+--
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: postgres
+--
+
+SELECT pg_catalog.setval('auth.user_id_seq', 2, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
